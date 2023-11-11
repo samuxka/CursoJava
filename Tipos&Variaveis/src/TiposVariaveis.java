ab50b8449bc0017de8b6eb76858cd4b7189c04f4
Samuel <samukactto@gmail.com>
2023-11-11 02:15:50 -0300

    oie

--- Tipos&Variaveis/src/TiposVariaveis.java
+++ Tipos&Variaveis/src/TiposVariaveis.java
@@ -6,7 +6,7 @@ public class TiposVariaveis {
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
-        short numeroCurto2 = (short) numeroNormal;
+        short numeroCurto2 = (short)numeroNormal;
 
         int numero = 5;
         numero = 10;
