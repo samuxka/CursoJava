35af6a469c3ed0893961f1ec96cfa53d60b09707
Samuel <samukactto@gmail.com>
2023-11-11 00:15:34 -0300

    aula tipos e variaveis

---
+++ Tipos&Variaveis/src/TiposVariaveis.java
@@ -0,0 +1,18 @@
+//int, byte, short, long, float, double, boolean e char "Não são considerados objetos"
+public class TiposVariaveis {
+
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        final double Valor_De_Pi = 3.14;
+
+        System.out.print(numero);
+    }
+}
--- Tipos&Variaveis/src/app.java
+++
@@ -1,5 +0,0 @@
-public class app {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Olá, Mundo!");
-    }
-}
