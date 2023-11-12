51610f2738f2f23386de0b5b610b4374524cd71b
Samuel <samukactto@gmail.com>
2023-11-12 03:33:18 -0300

    operadores

---
+++ Operadores/src/Operadores.java
@@ -0,0 +1,38 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //classe Operadores.java
+/*String nomeCompleto = "LINGUAGEM" + "JAVA";
+		
+//qual o resultado das expressoes abaixo?
+String concatenacao ="?"; 
+
+concatenacao = 1+1+1+"1";
+
+concatenacao = 1+"1"+1+1;
+
+concatenacao = 1+"1"+1+"1";
+
+concatenacao = "1"+1+1+1;
+
+concatenacao = "1"+(1+1+1);*/
+
+        /*int a, b;
+        a = 6;
+        b = 6;
+
+        int resultado = a == b ? 1 : 0;
+        System.out.println("resultado: " + resultado);*/
+
+        /*int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numero Um é maior e diferente o numero 2? " + simNao)
+        simNao = numero1 != numero2
+        System.out.println("numero Um é maior e diferente o numero 2? " + simNao)
+        simNao = numero1 > numero2
+        System.out.println("numero Um é maior e diferente o numero 2? " + simNao)
+        simNao = numero1 < numero2*/
+    }
+}
