1c7b7716f18dc7ffbd3a86185ced2a9651abd8b8
Samuel <samukactto@gmail.com>
2023-11-14 03:15:05 -0300

    007

---
+++ controleFluxo/src/resultadoEscolar.java
@@ -0,0 +1,10 @@
+public class resultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 9;
+
+        if (nota >= 7)
+            System.out.println("aprovado");
+        else
+            System.out.println("reprovado");
+    }
+}
